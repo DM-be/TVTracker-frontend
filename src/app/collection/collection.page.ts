1a4c8057d8d02eed50985f069006cd38955ae847
MorentDennis <dennis.morent@student.howest.be>
2020-05-29 18:29:57 +0200

    added subject to refreshmovies in collection

--- src/app/collection/collection.page.ts
+++ src/app/collection/collection.page.ts
@@ -12,7 +12,7 @@ import { Component } from '@angular/core';
 export class CollectionPage  {
 
 
-  public movies: Array<RadarrMovie>; // make new arrays instead of binding directly... issue in virtual list...
+  public movies: Array<RadarrMovie> = [];
 
   public filters: string [] = [];
   public possibleFilters: string [] = ['Downloaded', 'Grabbed', 'Monitored'];
@@ -23,48 +23,35 @@ export class CollectionPage  {
 
   async ngOnInit() {
    this.movies = this.movieService.getMoviesSubject().getValue();
-  }
-
-  async ionViewWillEnter() {
-    if(this.movies.length !== this.movieService.getMoviesSubject().getValue().length)
-    {
-      this.refreshMovies();
-    }
+   this.movieService.getMoviesSubject().subscribe(x => this.movies = [...x]);
+    // workaround because of failed change detection --> create new array;
+  // see https://github.com/ionic-team/ionic/issues/17371
   }
 
   private calculateApproxHeight() {
-    return ((window.innerWidth/4.5) * 1.5 + 11);
+    return ((window.innerWidth / 4.5) * 1.5 + 11);
   }
-
-  // workaround because of failed change detection --> create new array;
-  // see https://github.com/ionic-team/ionic/issues/17371
-  async refreshMovies() {
-    this.movies = [...this.movieService.getMoviesSubject().getValue()];
-  }
-
-
 
   public addFilter(filter: string) {
     this.filters.push(filter);
     const i = this.possibleFilters.findIndex(f => f.toLowerCase() === filter.toLowerCase());
     this.possibleFilters.splice(i, 1);
-    this.filterObject = {...this.filterObject,...{[filter.toLowerCase()]: true}}
+    this.filterObject = {...this.filterObject, ...{[filter.toLowerCase()]: true}};
     this.filterWithFilterObject();
-
   }
 
   public filterWithFilterObject() {
     this.movies = this.movies.filter((item) => {
-      for (var key in this.filterObject) {
-        if (item[key] === undefined || item[key] != this.filterObject[key])
+      for (const key in this.filterObject) {
+        if (item[key] === undefined || item[key] !== this.filterObject[key]) {
           return false;
+        }
       }
       return true;
     });
   }
 
-  public addTypeFilter(type: string)
-  {
+  public addTypeFilter(type: string) {
     this.filterType = type;
     this.filterByType();
   }
@@ -72,29 +59,22 @@ export class CollectionPage  {
   public resetFilterType() {
     this.filterType = undefined;
     this.filters = [];
-    this.refreshMovies();
   }
 
   public filterByType() {
-
     this.movies = this.movies.filter((item) => {
-     
-        if(this.filterType === 'movies')
-        {
+
+        if (this.filterType === 'movies') {
         //  return ((item instanceof RadarrMovie)
         }
 
     });
   }
-
-
-  public removeFilter(i: number)
-  {
+  public removeFilter(i: number) {
     const property = this.filters[i];
     delete this.filterObject[property.toLowerCase()];
     this.possibleFilters.push(property);
     this.filters.splice(i, 1);
-    this.refreshMovies();
     this.filterWithFilterObject();
   }
 
