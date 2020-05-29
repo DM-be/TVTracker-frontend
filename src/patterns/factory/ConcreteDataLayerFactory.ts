1530895ea0932b6356417f8865262440806d986e
MorentDennis <dennis.morent@student.howest.be>
2020-05-29 11:53:41 +0200

    implemented network service singleton in datalayer factory

--- src/patterns/factory/ConcreteDataLayerFactory.ts
+++ src/patterns/factory/ConcreteDataLayerFactory.ts
@@ -1,13 +1,19 @@
+import { NetworkService } from 'src/services/network/network.service';
 import { PouchDataLayer } from './PouchDataLayer';
 import { DataLayerFactory } from './DataLayerFactory';
 import { DataLayer } from './DataLayer';
 
 export class ConcreteDataLayerFactory extends DataLayerFactory {
 
-    createDataLayer(type: string): DataLayer {
+    createDataLayer(type: string, networkService: NetworkService): DataLayer {
         if (type === 'pouchdb') {
-            return new PouchDataLayer();
+            return new PouchDataLayer(networkService);
         }
+        else if(type === 'firestore')
+        {
+
+        }
+        
     }
 
 }
