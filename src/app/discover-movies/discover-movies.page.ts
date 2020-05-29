c89ab07438957ad31ce8cedceb3e16c061af3705
MorentDennis <dennis.morent@student.howest.be>
2020-05-29 19:45:17 +0200

    refactoring + cleanup discover movies

--- src/app/discover-movies/discover-movies.page.ts
+++ src/app/discover-movies/discover-movies.page.ts
@@ -1,12 +1,9 @@
-import { AddMovieCommand } from './../../patterns/command/AddMovieCommand';
 import { MovieService } from './../../services/movie/movie.service';
-import { Component, OnInit, ɵConsole } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TmdbMovie } from 'src/interfaces/TmdbMovie';
-import { AddMovieCommandOptions } from 'src/interfaces/AddMovieCommandOptions';
 import { LoadingController } from '@ionic/angular';
-import { Subscription } from 'rxjs';
 import { TmdbService } from 'src/services/tmdb/tmdb.service';
-import { TmdbMovieResultDTO } from 'src/interfaces/TmdbMovieResultDTO';
+
 
 @Component({
   selector: 'app-discover-movies',
@@ -18,41 +15,21 @@ export class DiscoverMoviesPage implements OnInit {
   public nowPlayingMovies: TmdbMovie [];
   public upComingMovies: TmdbMovie [];
 
-  constructor(private tmdbService: TmdbService, private movieService: MovieService, private loadingController: LoadingController) {
-  }
+  constructor(private tmdbService: TmdbService, private movieService: MovieService, private loadingController: LoadingController) {}
 
   ngOnInit() {
-    console.log('init called')
     this.initializeTmdbMovies();
   }
 
-  async addMovieToCollection(tmdbMovie: TmdbMovie) {
-
-    const addMovieCommandOptions: AddMovieCommandOptions = {
-      addOptions: {searchForMovie: false},
-      monitored: false,
-      qualityProfileId: 6,
-      tmdbId: tmdbMovie.tmdbId
-    };
-    const command = new AddMovieCommand(this.movieService, addMovieCommandOptions, tmdbMovie );
-    command.execute();
-    console.log("added movie!")
-  }
-  
-
   private async initializeTmdbMovies() {
     try {
-      this.movieService.dataLayerInitialisation().subscribe(async x => {
-        if(x)
+      this.movieService.dataLayerInitialisation().subscribe(async initialized => {
+        if(initialized)
         {
           this.nowPlayingMovies = await this.tmdbService.getNowPlayingTmdbMovies();
           this.upComingMovies = await this.tmdbService.getUpcomingTmdbMovies();
         }
-   
-      
       })
-      
-        
     } catch (error) {
       console.log(error);
     }
